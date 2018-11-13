407fdd81ae16b3ee58f8221f6f6f449f0f297099
rohan <rohan@qburst.com>
2018-11-13 10:54:25 +0530

    Completed nested comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,7 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :parent,  class_name: "Comment", optional: true   #-> requires "parent_id" column
   has_many   :replies, class_name: "Comment", foreign_key: "parent_id"
-  default_scope -> { order(created_at: :desc) }
+  default_scope -> { order(created_at: :asc) }
   validates :user_id, presence: true
   validates :post_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
       post 'ban_user'
     end
     resources :posts do
-      resources :comment
+      resources :comments
     end
   end
   root 'home#index'
