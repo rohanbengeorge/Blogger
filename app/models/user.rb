0ed11c95e636f09154ef382b80f62c45bdd34eb5
rohan <rohan@qburst.com>
2018-11-13 17:17:37 +0530

    added basic image uploading feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,10 @@ class User < ApplicationRecord
          :lockable
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :active_relationships, class_name:  "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name:  "Relationship",foreign_key: "followed_id",dependent: :destroy
+  has_many :following, through: :active_relationships,  source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   def account_active?
     return false if ban_till_date > Date.today
 
@@ -18,5 +22,20 @@ class User < ApplicationRecord
 
   def inactive_message
     account_active? ? super : 'Your Account is banned till ' + ban_till_date.to_s + ' for breaking our policies'
+  end
+
+  # Follows a user.
+  def follow(other_user)
+    following << other_user
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
   end
 end
